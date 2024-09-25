f9af1f3d1faa6c2f0084fb6f60a132f355c50494
moninch <142913779+moninch@users.noreply.github.com>
2024-09-25 13:33:50 +0300

    Добавил алгоритм с обнаружением syn,ack flood атак, сделала изъятие syn,ack флагов

--- app/extracting.py
+++ app/extracting.py
@@ -1,7 +1,7 @@
 import pyshark
 import pandas as pd
 
-def extract_features(pcap_file, label):
+def extract_features(pcap_file, label=0):
     capture = pyshark.FileCapture(pcap_file)
     features = []
     for packet in capture:
@@ -13,26 +13,45 @@ def extract_features(pcap_file, label):
             dst_ip = packet.ip.dst if hasattr(packet, 'ip') else None  # IP-адрес назначения
             src_port = packet[packet.transport_layer].srcport if hasattr(packet, packet.transport_layer) else None  # Порт источника
             dst_port = packet[packet.transport_layer].dstport if hasattr(packet, packet.transport_layer) else None  # Порт назначения
-            
-            features.append([protocol, length, time, src_ip, dst_ip, src_port, dst_port, label])
+            syn_flag = None
+            ack_flag = None
+
+            if protocol == 'TCP':
+                syn_flag = int(packet.tcp.flags_syn)
+                ack_flag = int(packet.tcp.flags_ack)
+
+            features.append([protocol, length, time, src_ip, dst_ip, src_port, dst_port, syn_flag, ack_flag, label])
         except AttributeError:
             continue
     capture.close()
     return features
 
+def label_ddos_attack(df, syn_threshold=10, ack_threshold=10):
+    for index, row in df.iterrows():
+        if row['SYN_Flag'] == 1 and row['ACK_Flag'] == 0:
+            if (df[(df['Src_IP'] == row['Src_IP']) & (df['SYN_Flag'] == 1)].shape[0] > syn_threshold):
+                df.at[index, 'Label'] = 1  #SYN-flood
+
+        elif row['ACK_Flag'] == 1 and row['SYN_Flag'] == 0:
+            if (df[(df['Src_IP'] == row['Src_IP']) & (df['ACK_Flag'] == 1)].shape[0] > ack_threshold):
+                df.at[index, 'Label'] = 1  #ACK-flood
+    
+    return df
 
 
-benign_features = extract_features("benign.pcap", 0)
-benign_pc_features = extract_features("benign-pc.pcapng", 0)
-cic_benign_features = extract_features("CIC-DDoS-2019-Benign.pcap", 0)
+benign_features = extract_features("benign.pcap")
+benign_pc_features = extract_features("benign-pc.pcapng")
+cic_benign_features = extract_features("CIC-DDoS-2019-Benign.pcap")
 
+syn_ack_features = extract_features("syn_ack_random.pcap", label=1)
+syn_flood_features = extract_features("syn_flood_25.pcap", label=1)
 
-syn_ack_features = extract_features("syn_ack_random.pcap", 1)
-syn_flood_features = extract_features("syn_flood_25.pcap", 1)
 all_features = benign_features + benign_pc_features + cic_benign_features + syn_ack_features + syn_flood_features
 
-df = pd.DataFrame(all_features, columns=['Protocol', 'Length', 'Time', 'Src_IP', 'Dst_IP', 'Src_Port', 'Dst_Port', 'Label'])
+df = pd.DataFrame(all_features, columns=['Protocol', 'Length', 'Time', 'Src_IP', 'Dst_IP', 'Src_Port', 'Dst_Port', 'SYN_Flag', 'ACK_Flag', 'Label'])
+
 df = pd.get_dummies(df, columns=['Protocol'])
 
-df.to_csv("traffic_dataset.csv", index=False)
+df = label_ddos_attack(df)
 
+df.to_csv("traffic_dataset.csv", index=False)
--- app/main.py
+++ app/main.py
@@ -10,30 +10,26 @@ app = FastAPI()
 model = tf.keras.models.load_model("packet_classifier_model.h5")
 
 class PacketData(BaseModel):
-    protocol: constr(regex='^(TCP|UDP|ICMP)$')  # Протокол может быть только TCP, UDP или ICMP
-    length: conint(gt=0)  # Длина пакета должна быть больше 0
-    time: condecimal(gt=0)  # Временная метка должна быть положительным числом
-    src_ip: constr(regex=r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')  # Проверка формата IP-адреса
+    protocol: constr(regex='^(TCP|UDP|ICMP)$')
+    length: conint(gt=0)
+    time: condecimal(gt=0)
+    src_ip: constr(regex=r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')
     dst_ip: constr(regex=r'^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')
-    src_port: conint(ge=0, le=65535)  # Порт должен быть в диапазоне от 0 до 65535
-    dst_port: conint(ge=0, le=65535)  # Порт назначения
-
-
-
+    src_port: conint(ge=0, le=65535)
+    dst_port: conint(ge=0, le=65535)
+    syn_flag: conint(ge=0, le=1)  
+    ack_flag: conint(ge=0, le=1)
 @app.post('/classify_packet')
 async def classify_packet(data: List[PacketData]):
     results = []
-
+    
     for packet in data:
         protocol_encoded = [1, 0] if packet.protocol == 'TCP' else [0, 1]
+        packet_data = [packet.length, packet.time, packet.syn_flag, packet.ack_flag] + protocol_encoded
 
-        packet  = [packet.length, packet.time] + protocol_encoded
-
-        packet = scaler.transform([packet])
-        prediction = model.predict(packet)
-        result = {"protocol": packet.protocol, "action": "accept" if prediction[0] == 0 else "drop"}
+        packet_data = scaler.transform([packet_data])
+        prediction = model.predict(packet_data)
+        result = {"protocol": packet.protocol, "action": "accept" if prediction[0][0] < 0.5 else "drop"}
         results.append(result)
-    
+
     return {"results": results}
-
-
