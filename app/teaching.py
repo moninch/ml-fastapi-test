521472ae82e10c66fb4bbe51e5d80929a3bda38b
moninch <142913779+moninch@users.noreply.github.com>
2024-09-27 20:50:45 +0300

    Добавил энкодеры для протоколов и айпишников, преобразовал все колонки в строки, точность модели на тестовых данных 0.6582914590835571

--- app/teaching.py
+++ app/teaching.py
@@ -2,13 +2,26 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder, StandardScaler
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("C:\Learning\ml-fastapi-test\\trafic\\traffic_dataset.csv")
 
 X = df.drop('label', axis=1)
 y = df['label']
+
+encoder = OrdinalEncoder()
+X_encoded = encoder.fit_transform(X[['ip.src', 'ip.dst']])
+X[['ip.src', 'ip.dst']] = X_encoded
+
+encoder = OneHotEncoder(handle_unknown='ignore')
+X_encoded = encoder.fit_transform(X[['_ws.col.protocol']]).toarray()
+
+X = pd.concat([X, pd.DataFrame(X_encoded)], axis=1)
+X = X.drop('_ws.col.protocol', axis=1)
+
+X.columns = X.columns.astype(str)
+
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
@@ -18,9 +31,9 @@ X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2)
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(64, activation='relu', input_shape=(X_train.shape[1],)),
-    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dropout(0.3),
     tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dropout(0.3),
     tf.keras.layers.Dense(1, activation='sigmoid')  # Выходной слой для бинарной классификации
 ])
 
